398435f54d69396f5c5ffe5ace90b8d0323e0c49
Artur Ishmuratov <ru_man1987@icloud.com>
2024-02-20 21:02:50 +0300

    Useing local storage to saveing token, removeing token,creating new user, debugging.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import Container from "@mui/material/Container";
 
 import { Header } from "./components";
 import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAuthMe, selectIsAuth } from "./redux/slices/auth";
+import React from "react";
 
 
 function App() {
+
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  React.useEffect(() => {
+    dispatch(fetchAuthMe());
+  }, [dispatch])
+
   return (
     <>
       <Header />
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -16,7 +16,6 @@ export const Login = () => {
   const {
     register,
     handleSubmit,
-    setError,
     formState: { errors, isValid },
   } = useForm({
     defaultValues: {
@@ -26,13 +25,22 @@ export const Login = () => {
     mode: 'onChange'
   });
 
-  const onSubmit = (values) => {
-    dispatch(fetchAuth(values));
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchAuth(values));
+
+    if (!data.payload) {
+      return alert('Не удалось авторизироваться');
+    }
+
+    if ('token' in data.payload) {
+      window.localStorage.setItem('token', data.payload.token);
+    }
   };
 
   if (isAuth) {
-    return <Navigate to= '/' />
-  }
+    return <Navigate to='/' />
+  };
+  
   
   return (
     <Paper classes={{ root: styles.root }}>
