b40fddbca7f2c59d97b92db14f235ab32734e649
Artur Ishmuratov <ru_man1987@icloud.com>
2024-05-16 15:52:36 +0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom"
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -9,6 +8,7 @@ import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 import store from "./redux/store";
+import { HashRouter } from "react-router-dom/dist";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -16,11 +16,11 @@ root.render(
   <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/MERNAppFrontend">
+      <HashRouter basename="/MERNAppFrontend">
         <Provider store={store}>
           <App />
         </Provider>
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </>
 );
